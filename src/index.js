cd2ba1f4cf0d94bc2757a0ec091604a1477918ee
Marta Zaorska <marta.zaorska2@gmail.com>
2019-06-08 19:36:54 +0200

    Basic PWA - add manifest.json, app icons, serviceWorker

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from './context';
 
 import './sass/index.scss';
 
-ReactDOM.render(<BrowserRouter><Provider><App /></Provider></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
+serviceWorker.register();
